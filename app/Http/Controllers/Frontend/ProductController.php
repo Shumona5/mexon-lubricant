1bfb26283ee4836c5f6cd371278f61273530a38f
Shumona5 <shumonashikha@gmail.com>
2024-04-28 17:02:33 +0600

    Update Product Controller

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -29,6 +29,95 @@ class ProductController extends Controller
             })->get();
             return view('frontend.pages.products.auto', compact('details', 'subdetails'));
         }
+        if ($slug == 'industrial') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+
+            return view('frontend.pages.products.industrial', compact('details', 'subdetails'));
+        }
+        if ($slug == 'marine-and-offshore') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+            return view();
+        }
+        if ($slug == 'speciality-grades') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+            return view();
+        }
+        if ($slug == 'motorbike') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+            return view('frontend.pages.products.motorbike',compact('details', 'subdetails'));
+        }
+        if ($slug == 'gasoline') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+            return view('frontend.pages.products.gasoline',compact('details', 'subdetails'));
+        }
+        if ($slug == 'diesel') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+            return view('frontend.pages.products.diesel',compact('details', 'subdetails'));
+        }
+        if ($slug == 'diesel-engine-2') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+            return view('frontend.pages.products.industrial-diesel',compact('details', 'subdetails'));
+        }
+        if ($slug == 'grease-2') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+            return view('frontend.pages.products.grease',compact('details', 'subdetails'));
+        }
+        if ($slug == 'transmission') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+            return view('frontend.pages.products.transmission.transmission',compact('details', 'subdetails'));
+        }
+        if ($slug == 'brake') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+            return view('',compact('details', 'subdetails'));
+        }
+        if ($slug == 'coolant') {
+            $details = ProductTypesDetails::all();
+            // $subdetails=SubProductTypesDetails::where('parent_id' , null)->get();
+            $subdetails = SubProductTypesDetails::whereHas('subproducts', function ($query) {
+                $query->where('parent_id', null);
+            })->get();
+            return view('',compact('details', 'subdetails'));
+        }
 
         
     }
